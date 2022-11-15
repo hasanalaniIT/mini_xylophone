bb5cdde7f1d40c2b9e9bb769684c8e74cb732eb7
Hasan Alani <59187679+hasanalaniIT@users.noreply.github.com>
2022-11-15 18:29:27 +0300

    Installing audio players package and testing it with a simple button and asset sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(Xylophone());
 
@@ -8,7 +9,15 @@ class Xylophone extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FloatingActionButton(
+              onPressed: () async {
+                final player = AudioPlayer();
+                await player.play(AssetSource('sounds/note1.wav'));
+              },
+              child: const Text("click"),
+            ),
+          ),
         ),
       ),
     );
